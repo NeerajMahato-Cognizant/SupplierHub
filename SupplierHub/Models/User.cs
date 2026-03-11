63f3e543da6af87dca068d96359737af9a3c88b9
Nayak <2478812@cognizant.com>
2026-03-11 11:29:17 +0530

    Some changes in user model

--- SupplierHub/Models/User.cs
+++ SupplierHub/Models/User.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using SupplierHub.Models;
+using SupplierHub.Constants.Enum;
 
 
 namespace SupplierHub.Models
